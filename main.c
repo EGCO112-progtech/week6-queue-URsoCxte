0401e503d819c2830b0cc182570d85fa23a352fb
URsoCxte <thanawat14433@gmail.com>
2025-02-12 08:50:03 +0000

    m

--- main.c
+++ main.c
@@ -8,23 +8,29 @@
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
    NodePtr tailPtr=NULL;
-/* For struct Queue
+
   Queue  q;
-   q. headPtr=NULL;
+   q.headPtr=NULL;
    q.tailPtr=NULL;
    q.size=0;
-*/
+
    int i,x;
    
 
  for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
+          x=dequeue_struct(&q);
+            if(x==-1){
+              printf("Empty queue\n");
+            }
+            else
+            {
+              printf("dequeing %d\n",x);
+            }
         }
         else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
+       enqueue(&q.headPtr,&q.tailPtr, atoi(argv[i]));
+           q.size++;
         }
  }
   return 0;
